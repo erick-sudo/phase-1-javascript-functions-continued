eed9e57269f9cfdb2dc89d62bb45853f1eb34c42
Erick Ochieng <erickochieng766@gmail.com>
2022-11-22 23:04:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrapper){
+    switch(wrapper){
+        case "*":
+            return function(){
+                return `You are ${wrapper}a hard worker${wrapper}!`
+            }
+        case "||":
+            return function(){
+                return `You are ${wrapper}a dedicated programmer${wrapper}!`
+            }
+    }
+}
